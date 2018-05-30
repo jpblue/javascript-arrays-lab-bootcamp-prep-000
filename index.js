ea56abfd4592bbb00d27043ccaa6d777610048d9
jayhpaq@yahoo.com <jayhpaq@yahoo.com>
2018-05-30 22:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function kittens() {
+var kittens = ["Milo", "Otis", "Garfield"];
+return kittens
+}
+
+function destructivelyAppendKitten(Ralph){
+  kittens.push(Ralph)
+  return kittens
+}
